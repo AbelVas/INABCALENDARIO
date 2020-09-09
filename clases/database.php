48c282c5cf4c47fe9da5254d16edcc1bf80c55db
AbelVas <56285352+AbelVas@users.noreply.github.com>
2020-09-09 08:49:38 -0600

    Update database.php

--- clases/database.php
+++ clases/database.php
@@ -1,8 +1,8 @@
 <?php
     class database{
         function conduc01(){
-            $con1=mysqli_connect("localhost","root","");
-            mysqli_select_db($con1,"brinab");
+            $con1=mysqli_connect("172.19.0.2","inab_calforestal","inab_calforestal");
+            mysqli_select_db($con1,"inab_calforestal");
             return($con1);
         }
     }
