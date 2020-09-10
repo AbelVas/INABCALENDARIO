7215f08e57de3df72b16d22d0e78b6f5935805ff
AbelVas <56285352+AbelVas@users.noreply.github.com>
2020-09-09 23:40:13 -0600

    logorainnew1

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
    <div class="text-center container">
         <img src="images/inabhorizontal.png" alt="" style="width:500px;height:120px;">
         <img src="images/reainfor.png" alt=""  style="width:200px;">
-        <img src="images/umg.png" alt=""  style="width:170px;height:170px;">
+        <img src="images/umg.png" alt=""  style="width:100px;height:100px;">
    
    </div>
    
