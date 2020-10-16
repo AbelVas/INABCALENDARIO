64bacc730555b588b9ce30408fa2804f323bd822
AbelVas <56285352+AbelVas@users.noreply.github.com>
2020-10-15 22:45:07 -0600

    asdfasdfasdf

--- includes/footer.php
+++ includes/footer.php
@@ -12,7 +12,7 @@
           <h3>Redes Sociales</h3>
           <a class="link-externos" href="https://www.facebook.com/institutonacionaldebosques" target="_blank" style="	padding: 15px;font-size: 25px;color:#fff;"><i class="fab fa-facebook-square"></i></a>
           <a class="link-externos" href="https://twitter.com/inabguatemala" target="_blank" style="	padding: 15px;font-size: 25px;color:#fff;"><i class="fab fa-twitter-square"></i></a>
-          <a class="link-externos" href="https://www.youtube.com/user/divulgacionINAB" target="_blank" style="	padding: 15px;font-size: 25px;color:#fff;"><i class="fab fa-youtube"></i></a>
+          <a class="link-externos" href="https://www.youtube.com/user/divulgacionINAB"  target="_blank" style="	padding: 15px;font-size: 25px;color:#fff;"><i class="fab fa-youtube"></i></a>
         </div>
         <!--Seccion 3-->
         <div class="col-sm-4 text-center">
